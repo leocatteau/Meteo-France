6c768cf1a5709f95223af227d7a6ced8254b52f2
leocatteau <leocatteau@gmail.com>
2025-04-01 15:30:19 +0200

    working dataloader(dataset(data))) with SampleMask + SequenceMask + WindowHorizon

---
+++ data_provider/data_preparation.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+import torch 
+from torch.utils.data import Dataset
+
+
+# these classes are the training preparation layers over the raw datasets, for liberty in masking processes, etc
+
+class WindowHorizonDataset(Dataset):
+    def __init__(self,args,data_source):
+        super(WindowHorizonDataset, self).__init__()
+        self.data, self.indices = data_source.numpy(return_idx=True)
+        self.window = args.window
+        self.horizon = args.horizon
+        self.scaler = args.scaler
+        self.mask = data_source.mask
+        self.coarse_frequency = self.window + self.horizon
+
+    def __getitem__(self, index):
+        sample = (self.data[index:index+self.window],self.data[index+self.window:index+self.window+self.horizon])
+        # shouldn't we introduce a mask? and give data from other stations in prediction time window ? 
+        # sample['x'] = self.data[index:index+self.window]
+        # sample['y'] = self.data[index+self.window:index+self.window+self.horizon]
+        if self.scaler is not None:
+            raise NotImplementedError("scaler not implemented yet.")
+        return sample
+    
+    def __len__(self):
+        return len(self.indices)
+
+    def __repr__(self):
+        return "{}(n_samples={})".format(self.__class__.__name__, len(self))
+    
+    def dataframe(self):
+        return pd.DataFrame(data=self.data, index=self.indices)
+    
+    def pytorch(self):
+        data = self.numpy()
+        return torch.FloatTensor(data)
+
+    
+class SequenceMaskDataset(Dataset):
+    def __init__(self,args,data_source):
+        super(SequenceMaskDataset, self).__init__()
+        self.data, self.indices = data_source.numpy(return_idx=True)
+        self.window = args.window
+        self.scaler = args.scaler
+        self.mask = data_source.mask
+        self.coarse_frequency = 1
+
+        # artificial masking
+        train_mask = np.array(np.random.rand(len(data_source), data_source.n_nodes) > args.mask_proba/self.window, dtype=bool)
+        masked_indices =  np.where(~train_mask)
+        for i,j in zip(masked_indices[0], masked_indices[1]):
+            start = max(0, i - int(0.5 * self.window))
+            end = min(self.window, i + int(0.5 * self.window))
+            train_mask[start:end, j] = False
+        train_mask[np.where(~self.mask)] = True
+        self.train_mask = train_mask
+        self.corrupted_data = self.data.copy()
+        self.corrupted_data[~self.train_mask] = np.nan
+
+    def __getitem__(self, index):
+        sample = (self.corrupted_data[index], self.data[index])
+        
+        if self.scaler is not None:
+            raise NotImplementedError("scaler not implemented yet.")
+        return sample
+    
+    def __len__(self):
+        return len(self.indices)
+
+    def __repr__(self):
+        return "{}(n_samples={})".format(self.__class__.__name__, len(self))
+    
+    def dataframe(self):
+        return pd.DataFrame(data=self.data, index=self.indices)
+    
+    def pytorch(self):
+        data = self.numpy()
+        return torch.FloatTensor(data)
+    
+
+class SampleMaskDataset(Dataset):
+    def __init__(self,args,data_source):
+        super(SampleMaskDataset, self).__init__()
+        self.data, self.indices = data_source.numpy(return_idx=True)
+        self.scaler = args.scaler
+        self.mask = data_source.mask
+        self.coarse_frequency = 1
+
+        # artificial masking
+        self.train_mask = np.zeros((len(data_source), data_source.n_nodes), dtype=bool)
+        self.train_mask[np.random.rand(len(data_source), data_source.n_nodes) > args.mask_proba] = 1
+        self.corrupted_data = self.data.copy()
+        self.corrupted_data[~self.train_mask] = np.nan
+
+    def __getitem__(self, index):
+        sample = (self.corrupted_data[index], self.data[index])
+        
+        if self.scaler is not None:
+            raise NotImplementedError("scaler not implemented yet.")
+        return sample
+    
+    def __len__(self):
+        return len(self.indices)
+
+    def __repr__(self):
+        return "{}(n_samples={})".format(self.__class__.__name__, len(self))
+    
+    def dataframe(self):
+        return pd.DataFrame(data=self.data, index=self.indices)
+    
+    def pytorch(self):
+        data = self.numpy()
+        return torch.FloatTensor(data)
---
+++ data_provider/data_provider.py
@@ -0,0 +1,51 @@
+import numpy as np
+from torch.utils.data import DataLoader, Subset
+
+from data_provider.data_factory import bdclim, bdclim_clean
+from data_provider.data_preparation import WindowHorizonDataset, SequenceMaskDataset, SampleMaskDataset
+
+# these classes are for creating the compatible data objects for the models, depending on the training process
+
+data_dict = {
+    'bdclim': bdclim,
+    'bdclim_clean': bdclim_clean
+    # 'Ornstein_Uhlenbeck': Ornstein_Uhlenbeck_Data,
+}
+dataset_dict = {
+    'SampleMaskDataset': SampleMaskDataset,
+    'SequenceMaskDataset': SequenceMaskDataset,
+    'WindowHorizonDataset': WindowHorizonDataset
+}
+
+class DataProvider:
+    def __init__(self, args):
+        self.args = args
+        self.batch_size = args.batch_size
+
+        assert args.data in data_dict, f"Data {args.data} not found in data dictionary: {data_dict.keys()}"
+        assert args.dataset in dataset_dict, f"Dataset {args.dataset} not found in dataset dictionary: {dataset_dict.keys()}"
+        Data = data_dict[args.data]
+        Dataset = dataset_dict[args.dataset]
+
+        self.data = Data(root_path=args.root_path,data_path=args.data_path,has_predictors=args.has_predictors)
+        self.dataset = Dataset(args=self.args, data_source=self.data)
+        
+        train_split = int(len(self.dataset) * 0.4)
+        val_split = int(len(self.dataset)  * 0.6)
+        test_split = int(len(self.dataset)  * 0.8)
+        # coarse indices by window size
+        train_indices = np.array([i for i in range(train_split) if i % self.dataset.coarse_frequency == 0])
+        val_indices = np.array([i for i in range(val_split) if i % self.dataset.coarse_frequency == 0])
+        test_indices = np.array([i for i in range(test_split) if i % self.dataset.coarse_frequency == 0])
+        self.train_dataset = Subset(self.dataset, train_indices)
+        self.val_dataset = Subset(self.dataset, val_indices)
+        self.test_dataset = Subset(self.dataset, test_indices)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset,batch_size=self.batch_size,shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset,batch_size=self.batch_size,shuffle=False)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset,batch_size=self.batch_size,shuffle=False)

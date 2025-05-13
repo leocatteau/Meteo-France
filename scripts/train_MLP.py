a72ce23de8ee719b4dcc3a9be221e2fe79d9180f
leocatteau <leocatteau@gmail.com>
2025-05-13 15:37:53 +0200

    manage results saving

--- scripts/train_MLP.py
+++ scripts/train_MLP.py
@@ -1,3 +1,6 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
 import json
 
 import sys
@@ -5,7 +8,7 @@ sys.path.append('..')
 
 ########################################################################
 from data_provider.data_provider import DataProvider
-from models.linear_MLP import linear_MLP
+from models.MLP import MLP
 from trainer.Filler import Filler
 
 from types import SimpleNamespace
@@ -19,7 +22,7 @@ def main(verbose=False):
     data_kwargs.data_path = 'bdclim_safran_2023-2024.nc'
     data_kwargs.has_predictors = False
     data_kwargs.scaler = None
-    data_kwargs.batch_size = 1
+    data_kwargs.batch_size = 10
     data_kwargs.mask_length = 24*7*3
     data_kwargs.mask_proba = 0.5
 
@@ -33,16 +36,16 @@ def main(verbose=False):
     filler_kwargs.epochs = 100
     filler_kwargs.keep_proba = 1-data_kwargs.mask_proba
 
-    filler = Filler(linear_MLP, model_kwargs, filler_kwargs)
+    filler = Filler(MLP, model_kwargs, filler_kwargs)
 
-    train_loss, test_loss = filler.train(train_dataloader=train_dataloader, test_dataloader=test_dataloader)
-    filler.save_model('../trained_models/linear_MLP.pt')
+    train_loss, test_loss = filler.train(train_dataloader=train_dataloader, test_dataloader=test_dataloader, verbose=False)
+    filler.save_model('../trained_models/MLP.pt')
 
     results = {
         'train_loss': train_loss,
         'test_loss': test_loss
     }
-    with open('../../results/linear_MLP.json', 'w') as file:
+    with open('../../results/train_MLP.json', 'w') as file:
         json.dump(results, file, indent=4)
 
 if __name__ == "__main__":
--- scripts/train_linear.py
+++ scripts/train_linear.py
@@ -1,4 +1,3 @@
-import torch
 import json
 
 import sys
@@ -6,51 +5,48 @@ sys.path.append('..')
 
 ########################################################################
 from data_provider.data_provider import DataProvider
-from models.GRIN import GRINet
+from models.linear import linear
 from trainer.Filler import Filler
 
 from types import SimpleNamespace
 
 
-def main():
+def main(verbose=False):
     data_kwargs = SimpleNamespace()
     data_kwargs.data = 'bdclim_clean'
-    data_kwargs.dataset = 'WindowHorizonDataset'
+    data_kwargs.dataset = 'SequenceMaskDataset'
     data_kwargs.root_path = '../../datasets/'
     data_kwargs.data_path = 'bdclim_safran_2023-2024.nc'
     data_kwargs.has_predictors = False
     data_kwargs.scaler = None
-    data_kwargs.batch_size = 15
+    data_kwargs.batch_size = 1
     data_kwargs.mask_length = 24*7*3
     data_kwargs.mask_proba = 0.5
-    data_kwargs.window = 24*1*1
-    data_kwargs.horizon = 0
 
     data_provider = DataProvider(data_kwargs)
-    adjacency_matrix = torch.FloatTensor(data_provider.data.umap_adjacency(threshold=0.1, verbose=False)).to('cuda:0' if torch.cuda.is_available() else 'cpu')
     train_dataloader = data_provider.train_dataloader()
     test_dataloader = data_provider.test_dataloader()
 
-    model_kwargs = dict(adj=adjacency_matrix, d_in=1)
+    model_kwargs = dict(seq_dim=data_provider.data.n_nodes)
     filler_kwargs = SimpleNamespace()
     filler_kwargs.lr = 1e-5
     filler_kwargs.epochs = 100
     filler_kwargs.keep_proba = 1-data_kwargs.mask_proba
 
-    filler = Filler(GRINet, model_kwargs, filler_kwargs)
+    filler = Filler(linear, model_kwargs, filler_kwargs)
 
     train_loss, test_loss = filler.train(train_dataloader=train_dataloader, test_dataloader=test_dataloader)
-    filler.save_model('../trained_models/GRINet.pt')
+    filler.save_model('../trained_models/linear.pt')
 
     results = {
         'train_loss': train_loss,
         'test_loss': test_loss
     }
-    with open('../../results/train_GRINet.json', 'w') as file:
+    with open('../../results/train_linear.json', 'w') as file:
         json.dump(results, file, indent=4)
 
-    print("Training completed. Results saved to ../../results/train_GRINet.json")
-
 if __name__ == "__main__":
-    main()
+    main(verbose=True)
+
+
 

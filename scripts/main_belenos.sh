e35d8d64c3b48e8fcf2f0cf65df749620832e52d
leocatteau <leocatteau@gmail.com>
2025-05-19 16:54:27 +0200

    belenos slurm

---
+++ scripts/main_belenos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --output=output.out
+#SBATCH --error=error.err
+#SBATCH --chdir /home/catteau/internship
+#SBATCH --job-name=main
+#SBATCH --partition gpu
+#SBATCH --gres gpu:1
+#SBATCH --mem 8G
+#SBATCH --cpus-per-task 1
+#SBATCH --time=1:00:00
+
+# Load all modules
+module load gcc
+module load python 
+module load cuda 
+module load cudnn
+
+# Activate the environment
+source .venv/bin/activate
+cd Meteo-France/scripts
+
+#srun python experiments_main.py
+#srun --partition gpu --qos gpu --gres gpu:1 --mem 8G --cpus-per-task 5 --time 0-1:00:00 python -m python train_neural_net
+# srun python -m train_linear_MLP
+# srun python -m train_GRIN
+srun python -m reconstruct_GRIN
+# srun python -m hallucination_GRIN

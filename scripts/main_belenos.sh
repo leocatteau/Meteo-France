bb70384c595a4bd07ca59d651f21586b97b213ae
CATTEAU Leo <catteaul@sxcen>
2025-05-20 10:21:35 +0200

    data on belenos

--- scripts/main_belenos.sh
+++ scripts/main_belenos.sh
@@ -3,10 +3,10 @@
 #SBATCH --error=error.err
 #SBATCH --chdir /home/catteau/internship
 #SBATCH --job-name=main
-#SBATCH --partition gpu
-#SBATCH --gres gpu:1
-#SBATCH --mem 8G
-#SBATCH --cpus-per-task 1
+#SBATCH --partition=ndl
+#SBATCH --mem-per-cpu=8G
+# SBATCH --nodes=1
+# SBATCH --ntasks-per-node=16
 #SBATCH --time=1:00:00
 
 # Load all modules
@@ -19,9 +19,10 @@ module load cudnn
 source .venv/bin/activate
 cd Meteo-France/scripts
 
+data_path="/scratch/work/catteaul/datasets/"
+
 #srun python experiments_main.py
-#srun --partition gpu --qos gpu --gres gpu:1 --mem 8G --cpus-per-task 5 --time 0-1:00:00 python -m python train_neural_net
-# srun python -m train_linear_MLP
-# srun python -m train_GRIN
-srun python -m reconstruct_GRIN
+srun python -m train_linear $data_path
+# srun python -m train_GRIN $data_path
+# srun python -m reconstruct_GRIN
 # srun python -m hallucination_GRIN
